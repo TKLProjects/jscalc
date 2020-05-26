f72ff40549f04b74b16215a0947e895f932de1c8
theikegeraads <theike2@gmail.com>
2020-05-26 21:51:17 +0200

    finished most things (+/-, % and 0 dont work)

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function add1(){
-    let resultText = document.getElementById("result")
-    resultText.innerHTML = "dieke"
-}
-function add2()
-function add3()
-function add4()
-function add5()
-function add6()
-function add7()
-function add8()
-function add9()
+// this isnt used yet i want to put all the code in here but that doesn't work yet
