8a276d771628508263af8ff8c36db15d1c00c7d0
theikegeraads <theike2@gmail.com>
2020-05-26 21:06:14 +0200

    tried to make it work (dont use its very broken)

---
+++ index.js
@@ -0,0 +1,12 @@
+function add1(){
+    let resultText = document.getElementById("result")
+    resultText.innerHTML = "dieke"
+}
+function add2()
+function add3()
+function add4()
+function add5()
+function add6()
+function add7()
+function add8()
+function add9()
